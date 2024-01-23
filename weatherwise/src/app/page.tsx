3c0131ccb5ecaba70ed999eff290de191cda3abe
Rishikesh <rishikeshjagadale19@gmail.com>
2024-01-23 16:39:02 +0530

    feat: Add Frontend and Styling
    Implemented the frontend structure and styling for the WeatherWise application. Introduced components for weather display, search bar, and additional information sections. Applied responsive design principles to enhance user experience across different devices. Integrated weather icons for various weather conditions. Styling improvements include background images, color schemes, and layout adjustments. Ready to showcase the weather information in a visually appealing manner.

--- weatherwise/src/app/page.tsx
+++ weatherwise/src/app/page.tsx
@@ -4,16 +4,29 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import { useEffect, useState } from "react";
 
+import SearchIcon from "@mui/icons-material/Search";
+import FilterDramaIcon from "@mui/icons-material/FilterDrama";
+import DehazeIcon from "@mui/icons-material/Dehaze";
+import VapingRoomsIcon from "@mui/icons-material/VapingRooms";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
+
 //
 let WEATHER_API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
+//console.log("All Environment Variables:", process.env);
+
 //
 export default function Home() {
   //
-  const [place, setPlace] = useState("Pune");
+  const [place, setPlace] = useState();
+  const [placeData, setPlaceData] = useState<any>(null);
+  const currentTime = new Date().toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
 
   //
   const getWeatherData = async () => {
-    // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+    //https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=WEATHER_API_KEY
 
     if (place && place.length > 0) {
       try {
@@ -21,6 +34,7 @@ export default function Home() {
         let res = await fetch(url);
         let data = await res.json();
         console.log("GET WEATHER DATA RESPONSE ", data);
+        setPlaceData(data);
       } catch (err) {
         console.log(err);
       }
@@ -32,8 +46,94 @@ export default function Home() {
   }, []);
   //
   return (
-    <div>
-      <h1>Hey, Welcome to WeatherWise !!</h1>
+    <div className={styles.maindiv}>
+      <div className={styles.searchBar}>
+        <input
+          type="search"
+          placeholder="Enter the city name"
+          onChange={(e) => setPlace(e.target.value)}
+        />
+        <button onClick={getWeatherData}>
+          <SearchIcon />
+        </button>
+      </div>
+
+      {placeData && (
+        <div className={styles.row}>
+          <div className={styles.sectionA}>
+            <div className={styles.sectionA1}>
+              {placeData.weather[0].main === "Clouds" && (
+                <FilterDramaIcon className={styles.weatherIcon} />
+              )}
+              {placeData.weather[0].main === "Haze" && (
+                <DehazeIcon className={styles.weatherIcon} />
+              )}
+              {placeData.weather[0].main === "Smoke" && (
+                <VapingRoomsIcon className={styles.weatherIcon} />
+              )}
+              {placeData.weather[0].main === "Clear" && (
+                <WbSunnyIcon className={styles.weatherIcon} />
+              )}
+
+              <p className={styles.temp}>
+                {(placeData?.main.temp - 273.15).toFixed(1)} <span>°C</span>
+              </p>
+            </div>
+            <div className={styles.sectionA1}>
+              <p className={styles.city}>{placeData?.name}</p>
+              <p className={styles.weathertype}>{placeData?.weather[0].main}</p>
+            </div>
+          </div>
+
+          <div className={styles.timediv}>
+            <p className={styles.time}>{currentTime}</p>
+          </div>
+        </div>
+      )}
+
+      {placeData && (
+        <div className={styles.sectionB}>
+          <div className={styles.sectionB1}>
+            <p className={styles.head1}>Temperature</p>
+            <p className={styles.head2}>
+              {(placeData?.main.temp - 273.15).toFixed(1)} °C
+            </p>
+          </div>
+
+          <div className={styles.sectionB1}>
+            <p className={styles.head1}>Temperature Min</p>
+            <p className={styles.head2}>
+              {(placeData?.main.temp_min - 273.15).toFixed(1)} °C
+            </p>
+          </div>
+
+          <div className={styles.sectionB1}>
+            <p className={styles.head1}>Temperature Max</p>
+            <p className={styles.head2}>
+              {(placeData?.main.temp_max - 273.15).toFixed(1)} °C
+            </p>
+          </div>
+
+          <div className={styles.sectionB1}>
+            <p className={styles.head1}>Humidity</p>
+            <p className={styles.head2}>{placeData?.main.humidity}</p>
+          </div>
+
+          <div className={styles.sectionB1}>
+            <p className={styles.head1}>Pressure</p>
+            <p className={styles.head2}>{placeData?.main.pressure}</p>
+          </div>
+
+          <div className={styles.sectionB1}>
+            <p className={styles.head1}>Visibility</p>
+            <p className={styles.head2}>{placeData?.visibility}</p>
+          </div>
+          <div className={styles.sectionB1}>
+            <p className={styles.head1}>Wind Speed</p>
+            <p className={styles.head2}>{placeData?.wind.speed} km/hr</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
